b051752b5f7016f2310a90f80fcdc832083feaf3
LRaposoRocha <lrrocha2003@gmail.com>
2025-02-26 15:12:06 -0300

    Commit da Criação da Tela de Empréstimos Ativos

---
+++ telaEmprestimosAtivos.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import *
+
+root_EA = tk.Tk()
+root_EA.title = "Empréstimo de Materiais NPE"
+root_EA.geometry("900x600")
+root_EA.resizable(False,False)
+
+frame_EA = tk.Frame(root_EA)
+
+root_EA.mainloop()
