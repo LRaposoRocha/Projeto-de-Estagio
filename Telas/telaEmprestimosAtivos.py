01d4aae1e3c81071825f4aed76324a1b4ae72b2a
LRaposoRocha <lrrocha2003@gmail.com>
2025-03-06 17:41:23 -0300

    Criação da tela de cadastro de alunos e da pasta responsável por armazenar todas as telas

---
+++ Telas/telaEmprestimosAtivos.py
@@ -0,0 +1,27 @@
+# PROJETO DE ESTÁGIO - UNIVERSIDADE SANTA CECÍLIA 2025
+# ALUNO: LEONARDO RAPOSO ROCHA - R.A.: 201764
+# ESTE PROJETO CONSISTE EM CRIAR UM APLICATIVO RESPONSÁVEL POR SUBSTITUIR O USO DAS FICHAS DE AUTORIZAÇÃO DO LABORATÓRIO M513.
+
+import tkinter as tk
+from tkinter import ttk
+
+root_EA = tk.Tk()
+root_EA.title = "Empréstimo de Materiais NPE"
+root_EA.geometry("1100x619")
+root_EA.resizable(False,False)
+
+frame_Tabela = tk.Frame(root_EA)
+frame_Tabela.pack()
+
+label_Titulo = tk.Label(frame_Tabela, text = "EMRPÉSTIMOS ATIVOS")
+label_Titulo.pack(pady = (100, 30))
+
+colunas_EA = ("Data", "Hora", "Nome", "RA", "Curso")
+tabela_EA = ttk.Treeview(root_EA, columns = colunas_EA, show = "headings")
+
+for colunas in colunas_EA:
+    tabela_EA.heading(colunas, text=colunas)
+
+tabela_EA.pack()
+
+root_EA.mainloop()
